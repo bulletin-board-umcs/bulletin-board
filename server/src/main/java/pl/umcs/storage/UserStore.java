1368c3088e3193f153c34f478164622fa6ee5b10
michal <pkot2003@gmail.com>
2026-06-08 22:38:41 +0200

    Remove unused methods.

--- server/src/main/java/pl/umcs/storage/UserStore.java
+++ server/src/main/java/pl/umcs/storage/UserStore.java
@@ -95,16 +95,6 @@ public class UserStore {
         }
     }
 
-//    public void addUser(User user) {
-//        lock.writeLock().lock();
-//        try {
-//            users.put(user.getUsername(), user);
-//            persist();
-//        } finally {
-//            lock.writeLock().unlock();
-//        }
-//    }
-
     public String hash(String text) {
         return Integer.toHexString(text.hashCode());
     }
